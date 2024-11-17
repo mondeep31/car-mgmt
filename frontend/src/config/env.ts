ce13e35308e5bdba9ea15a85a696a8b25e6e5d90
Mondeep Chetry <mandeepchetry6969@gmail.com>
2024-11-17 18:48:11 +0530

    removed exposed url from FE

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,5 +1,5 @@
 // Environment configuration is now managed in config/config.ts
 export const env = {
-    // BACKEND_URL: import.meta.env.VITE_API_URL || 'https://team-mgmt-backend.el.r.appspot.com',
+
     BACKEND_URL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
   };
